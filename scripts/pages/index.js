68167eab70e6814eecae3c5b2c1c32e457e88d83
JK <jk.floods@gmail.com>
2022-07-04 18:40:12 +0200

    Load photographers datas with fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,16 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        fetch("../data/photographers.json")
+            .then(function(res) {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+            .then(function(data) {
+                displayData(data.photographers);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
     }
 
     async function displayData(photographers) {
@@ -36,9 +24,8 @@
     };
 
     async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        // Récupère les datas des photographes et les affiche
+        await getPhotographers();
     };
     
     init();
