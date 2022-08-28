eee6f39786e1fcde366c87025cd9cf219e407667
JK <jk.floods@gmail.com>
2022-08-29 00:36:22 +0200

    Trap focus inside modal
    https://uxdesign.cc/how-to-trap-focus-inside-modal-to-make-it-ada-compliant-6a50f9a70700?gi=6b76cba96fb0

--- scripts/models/Photographer.js
+++ scripts/models/Photographer.js
@@ -117,6 +117,12 @@ class Photographer {
         const content      = document.querySelector('.lightbox__content')
         const nextLink     = document.querySelector('.lightbox__link--right')
         const previousLink = document.querySelector('.lightbox__link--left')
+        
+        // @see https://uxdesign.cc/how-to-trap-focus-inside-modal-to-make-it-ada-compliant-6a50f9a70700
+        const focusableElements     = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+        const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+        const focusableContent      = modal.querySelectorAll(focusableElements);
+        const lastFocusableElement  = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
         for (let i in this._medias) {
             var media = this._medias[i]
@@ -186,5 +192,27 @@ class Photographer {
         }
 
         modal.style.display = 'flex'
+
+        document.addEventListener('keydown', function(e) {
+            let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+          
+            if (!isTabPressed) {
+              return;
+            }
+          
+            if (e.shiftKey) { // if shift key pressed for shift + tab combination
+              if (document.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus(); // add focus for the last focusable element
+                e.preventDefault();
+              }
+            } else { // if tab key is pressed
+              if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+                firstFocusableElement.focus(); // add focus for the first focusable element
+                e.preventDefault();
+              }
+            }
+          });
+          
+          firstFocusableElement.focus();
     }
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,31 @@
 function displayModal() {
     const modal = document.querySelector('.modal_background')
-    modal.style.display = "block"
+    const firstFocusableElement = document.getElementById('firstName')
+    const lastFocusableElement = document.querySelector('.contact_modal__btnClose')
 
-    const firstElement = document.getElementById('firstName')
-    firstElement.focus()
+    document.addEventListener('keydown', function(e) {
+        let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+        if (!isTabPressed) {
+            return;
+        }
+
+        if (e.shiftKey) { // if shift key pressed for shift + tab combination
+            if (document.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus(); // add focus for the last focusable element
+                e.preventDefault();
+          }
+        } else { // if tab key is pressed
+            if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+                console.log('last');
+                firstFocusableElement.focus(); // add focus for the first focusable element
+                e.preventDefault();
+            }
+        }
+    });
+
+    modal.style.display = "block"
+    firstFocusableElement.focus();
 }
 
 function closeModal() {
